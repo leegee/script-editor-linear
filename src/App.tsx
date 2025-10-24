34f3935cb871822f786200265efa036388cf13c3
Lee Goddard <leegee@gmail.com>
2025-10-24 13:43:04 +0200

    Do not autoload

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { createSignal, onMount, ParentProps, Show } from "solid-js";
 import AlertConfirm from "./components/modals/AlertConfirm";
-import { storage } from "./db";
-import { loadSampleScript } from "./lib/io";
-import { loadAll, orderedItems } from "./stores";
+import { loadAll } from "./stores";
 
 export default function App(props: ParentProps) {
     const [loaded, setLoaded] = createSignal(false);
 
     onMount(async () => {
         if (!loaded()) {
-            const keys = await storage.getKeys("timelineItems");
-            if (!keys?.length) await loadSampleScript();
             await loadAll();
             setLoaded(true);
         }

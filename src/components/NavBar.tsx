06548a5f042e026ce641050bf6cc9d3fe12009a0
Lee Goddard <leegee@gmail.com>
2025-10-28 15:25:42 +0100

    smaller

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,17 +6,17 @@ interface NavBarProps {
 
 export default function Navbar(props: NavBarProps) {
     return (
-        <nav class="top-align surface-container middle-align small-padding bottom-margin top-margin">
-            <button class="transparent no-border padding">
-                <A class="transparent " href={`${props.root}/locations`}><i class="extra small-opacity">location_on</i></A>
+        <nav class="top-align surface-container middle-align padding bottom-margin top-margin">
+            <button class="transparent no-border tiny-padding">
+                <A class="transparent " href={`${props.root}/locations`}><i class="large small-opacity">location_on</i></A>
                 <div class="tooltip bottom">Locations</div>
             </button>
-            <button class="transparent no-border padding">
-                <A class="transparent " href={`${props.root}/characters`}><i class="extra small-opacity">people</i></A>
+            <button class="transparent no-border tiny-padding">
+                <A class="transparent " href={`${props.root}/characters`}><i class="large small-opacity">people</i></A>
                 <div class="tooltip bottom">Characters</div>
             </button>
-            <button class="transparent no-border padding">
-                <A class="transparent " href={`${props.root}/filters`}><i class="extra small-opacity">filter_alt</i></A>
+            <button class="transparent no-border tiny-padding">
+                <A class="transparent " href={`${props.root}/filters`}><i class="large small-opacity">filter_alt</i></A>
                 <div class="tooltip bottom">Filters</div>
             </button>
         </nav>

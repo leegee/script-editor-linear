abb30cc8308b4083193cbc03b61a29c7b6c3e7c5
Lee Goddard <leegee@gmail.com>
2025-10-30 10:46:57 +0100

    beat type

--- src/components/CoreItems/BeatItem.tsx
+++ src/components/CoreItems/BeatItem.tsx
@@ -3,7 +3,7 @@ import { TimelineItem, TimelineItemProps } from "./TimelineItem";
 
 export class BeatItem extends TimelineItem {
     constructor(props: TimelineItemProps) {
-        super({ ...props, type: "lighting" });
+        super({ ...props, type: "beat" });
         this.details = {
             ...this.details,
             doesNotAdvanceTime: true,

dc6ab470d1ab363e0263ffa6e723332514ff3b1f
Lee Goddard <leegee@gmail.com>
2025-11-03 15:24:00 +0100

    Remoe help tab

--- src/components/panels/SettingsView.tsx
+++ src/components/panels/SettingsView.tsx
@@ -4,36 +4,14 @@ export default function SettingsView() {
     const [activeTab, setActiveTab] = createSignal("options");
 
     return (
-        <article>
-
+        <div class="active page">
             <header>
                 <h2>Settings</h2>
             </header>
 
-            <nav class="tabbed small medium-margin margin-bottom">
-                <a
-                    class={activeTab() === "options" ? "active" : ""}
-                    onClick={() => setActiveTab("options")}
-                >
-                    <i>settings</i>
-                    <span>Options</span>
-                </a>
-                <a
-                    class={activeTab() === "help" ? "active" : ""}
-                    onClick={() => setActiveTab("help")}
-                >
-                    <i>help</i>
-                    <span>Help</span>
-                </a>
-            </nav>
-
-            <div class={`page padding ${activeTab() === "options" ? "active" : ""}`}>
+            <article class="responsive">
                 <p>Options go here…</p>
-            </div>
-
-            <div class={`page padding ${activeTab() === "help" ? "active" : ""}`}>
-                <p>Help information here…</p>
-            </div>
-        </article>
+            </article>
+        </div>
     );
 }

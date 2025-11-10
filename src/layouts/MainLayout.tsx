4cc0cb384f0a48999ca81d0fc9d41bbe6dc18b71
Lee Goddard <leegee@gmail.com>
2025-11-10 15:36:37 +0100

    Tooltips

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -11,9 +11,18 @@ export default function MainLayout(props: ParentProps) {
             <main style="height: 100vh" class="background no-padding no-margin">
                 <div class="tabs max background bottom-margin small-margin">
                     <Menu />
-                    <A title="Script view" class="transparent" href="/script"><i class="small">list_alt</i></A>
-                    <A title="Timeline view" class="transparent" href="/timeline"><i class="small">view_timeline</i></A>
-                    <A title="Typing view" class="transparent" href="/typing"><i class="small">keyboard</i></A>
+                    <A title="Script view" class="transparent" href="/script">
+                        <i class="small">list_alt</i>
+                        <div class="tooltip bottom">List View</div>
+                    </A>
+                    <A title="Timeline view" class="transparent" href="/timeline">
+                        <i class="small">view_timeline</i>
+                        <div class="tooltip bottom">Timeline View</div>
+                    </A>
+                    <A title="Typing view" class="transparent" href="/typing">
+                        <i class="small">keyboard</i>
+                        <div class="tooltip bottom">Typing View</div>
+                    </A>
                 </div>
 
                 <div>

01ee619fd9e088fc7ca3a42d5daec193a1151ace
Lee Goddard <leegee@gmail.com>
2025-11-11 13:32:13 +0100

    Accept JSX

--- src/stores/modals.ts
+++ src/stores/modals.ts
@@ -1,12 +1,12 @@
-import { createSignal } from "solid-js";
+import { createSignal, type JSX } from "solid-js";
 
-type AlertState = { message: string; resolve?: () => void } | null;
-type ConfirmState = { message: string; resolve: (value: boolean) => void } | null;
+type AlertState = { message: string | JSX.Element; resolve?: () => void } | null;
+type ConfirmState = { message: string | JSX.Element; resolve: (value: boolean) => void } | null;
 
 export const [alertState, setAlertState] = createSignal<AlertState>(null);
 export const [confirmState, setConfirmState] = createSignal<ConfirmState>(null);
 
-export function showAlert(message: string) {
+export function showAlert(message: string | JSX.Element) {
     return new Promise<void>((resolve) => {
         setAlertState({ message, resolve });
     });
@@ -18,7 +18,7 @@ export function closeAlert() {
     setAlertState(null);
 }
 
-export function showConfirm(message: string) {
+export function showConfirm(message: string | JSX.Element) {
     return new Promise<boolean>((resolve) => {
         setConfirmState({ message, resolve });
     });

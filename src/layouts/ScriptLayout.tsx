9ea9d60a8eba74adc12147e88766eebe810c8d71
Lee Goddard <leegee@gmail.com>
2025-11-14 09:39:30 +0100

    tidy

--- src/layouts/ScriptLayout.tsx
+++ src/layouts/ScriptLayout.tsx
@@ -4,14 +4,24 @@ import ScriptView from "../components/views/ScriptView";
 export default function ScriptLayout(props: ParentProps) {
     return (
         <>
-            <main class="responsive" style="overflow:hidden; height: 100vh">
-                <div class="grid" style="height: calc(100vh - 7em); overflow: hidden ">
-                    <div class="surface-container-low s12 m8 l8" style='height: auto; overflow-y: auto'>
+            <main class="responsive"
+                style="overflow:hidden; height: 100vh"
+            >
+                <div class="grid"
+                    style="height: calc(100vh - 7em); overflow: hidden"
+                >
+                    <div class="surface-container-low s12 m8 l8"
+                        style='height: auto; overflow-y: auto'
+                    >
                         <ScriptView />
                     </div>
 
-                    <div class="surface-container-low s12 m4 l4 small-padding right-padding left-padding" style="overflow-y: auto; height:100%; display:flex; flex-direction: column;">
-                        <fieldset class="surface-container top-padding; overflow-y:auto">
+                    <div class="surface-container-low s12 m4 l4 small-padding right-padding left-padding"
+                        style="overflow-y: auto; height:100%; display:flex; flex-direction: column"
+                    >
+                        <fieldset class="surface-container top-padding"
+                            style="overflow-y:auto; overflow-x: hidden"
+                        >
                             {props.children}
                         </fieldset>
                     </div>

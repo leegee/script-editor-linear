73f75ad1a1553c130fb9d733d23a61d8d113302e
Lee Goddard <leegee@gmail.com>
2025-11-17 12:45:16 +0100

    Simplify

--- src/components/views/TypingView.tsx
+++
@@ -1,5 +0,0 @@
-import TypingEditor from "../TypingInput";
-
-export default function TypingView() {
-    return <TypingEditor />
-}
--- src/layouts/TypingLayout.tsx
+++ src/layouts/TypingLayout.tsx
@@ -1,5 +1,5 @@
 import { type ParentProps } from "solid-js";
-import TypingView from "../components/views/TypingView";
+import TypingInput from "../components/TypingInput";
 
 export default function ScriptLayout(props: ParentProps) {
     return (
@@ -7,7 +7,7 @@ export default function ScriptLayout(props: ParentProps) {
             <main class="responsive" style="overflow:hidden; height: 100vh">
                 {/* <div class="grid" style="height: calc(100vh - 7em); overflow: hidden ">
                     <div class="s12 m8 l8" style='height: auto; overflow-y: auto'> */}
-                <TypingView />
+                <TypingInput />
                 {/* </div>
 
                     <div class="s12 m4 l4 small-padding right-padding left-padding" style="height:100%; overflow:hidden;   display: flex; flex-direction: column;">
